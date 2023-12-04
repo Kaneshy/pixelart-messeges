5b7d90a2ea1b424622119da41f42dea950c717ad
david <kaneshiuchiha@gmail.com>
2023-12-04 17:16:00 -0500

    ad commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
- 
+
 export default function Home() {
   return (
-    <h1 className="head-text">HOME</h1>
+    <div className="flex flex-col text-center justify-center p-4">
+      <h1 className="head-text">HOME</h1>
+
+      <img className="w-full " src="https://res.cloudinary.com/dh01ngdjo/image/upload/v1700921735/my-uploads/psenujp1y7v0fmwgygly.png" alt="" />
+    
+    </div>
+
   )
 }
